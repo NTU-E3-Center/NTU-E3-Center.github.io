a89a03240ae6a867d59e06c795062bc70ad19370
Yuan-Hsi Chien <91683644+dobahsi@users.noreply.github.com>
2025-07-13 19:46:36 +0800

    Update g-scholar-scrape.py

--- assets/modules/g-scholar-scrape.py
+++ assets/modules/g-scholar-scrape.py
@@ -5,7 +5,7 @@ import requests
 
 # Get your API key at https://serpapi.com/dashboard
 # addd
-api_key = "9564b246509b8b5c0be41828e72e1793ddd3dafb1a6c3ba9a4c41114fb0b2c5d"
+api_key = "9564b246509b8b5c0be41828e72e1793ddd3dafb1a6c3ba9a4c41114fb0b2c5"
 
 # Define the output file path
 output_path = '../src/pub.json'
